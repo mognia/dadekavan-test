6af0610c9e6e3cb3d39be3f6a62c858f7d9e9ee0
mognia <mognia420@gmail.com>
2019-06-11 13:26:34 +0430

    change user details when select eachone

--- src/app/components/user-address/user-address.component.ts
+++ src/app/components/user-address/user-address.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-address',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-address.component.css']
 })
 export class UserAddressComponent implements OnInit {
-
+  @Input() user :any;
   constructor() { }
 
   ngOnInit() {
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -11,17 +11,26 @@ export class ApiService {
     {
       name : 'علی بلیانی',
       phone: '09171171586',
-      photo:'/img_avatar.png'
+      photo:'/img_avatar.png',
+      city:'اهواز',
+      address:'خیابان اول کوچه دوم بن بست یکم پلاک 77',
+      mail:'a@b.com'
     },
     {
       name : 'محمدرضا غفارنیا',
       phone: '09101722342',
-      photo:'./assets/avatars/avatar.jpg'
+      photo:'./assets/avatars/avatar.jpg',
+      city:'تهران',
+      address:'میدان پونک - میدان پونک - کوچه گلزار 3 - پلاک 1 - طبقه 2 - واحد 4',
+      mail:'mohammad@ghaffarnia.com'
     },
     {
       name : 'کاربر تست',
       phone: '0912345667',
-      photo:'./assets/avatars/avatar2.png'
+      photo:'./assets/avatars/avatar2.png',
+      city:'نیویورک',
+      address:'خیابان اول دست راست',
+      mail:'my@addres.com'
     },
     
     ];
